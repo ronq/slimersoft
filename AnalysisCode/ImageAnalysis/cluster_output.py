31b95e08126299e75338a99f83d059ebdf537c3d
Michael Ronquest <ronquest@lanl.gov>
2013-08-05 11:46:47 -0600

    Now have a dedicated cluster output object. Still need to access it properly for HD5 output.

---
+++ AnalysisCode/ImageAnalysis/cluster_output.py
@@ -0,0 +1,13 @@
+class cluster_output:
+        """
+        """ 
+        def __init__(self):
+            self.outputClusterSize=-999
+            self.outputCounts=-999
+            self.outputClusterFrac=-999
+            self.outputAvgPixelCount=-999
+            self.outputPosition=-999
+            self.outputPositionVariance=-999
+            self.outputPeakHeight=-999
+            self.outputNumberOfClusters=-999
+            return    
